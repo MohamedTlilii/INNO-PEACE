103340994b66e1c894d316a0851d0c51235e058d
NAUT5 <Mohamedtlili1995@hotmail.com>
2024-03-09 02:01:21 +0100

    navbar

--- src/Components/NavbarFromBootsTrap.js
+++ src/Components/NavbarFromBootsTrap.js
@@ -4,40 +4,52 @@ import "./Style.css";
 
 function NavbarFromBootstrap() {
   return (
-    <Navbar
-      className="NavbarFromBootsTrap"
-      expand="lg"
-      style={{ height: "200px", backgroundColor: "white" }}
-    >
-      <Container>
-        <Navbar.Brand href="#home">
-          <img
-            src="/assets/ino.png"
-            height="360"
-            className="d-inline-block align-top"
-            alt="React Bootstrap logo"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className="aaaa" id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div>
+      <Navbar
+        className="NavbarFromBootsTrap"
+        expand="lg"
+        style={{
+          height: "350px",
+          //  backgroundColor: "hsl(197, 78%, 50%)"
+        }}
+      >
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src="/assets/ino.png"
+              height="360"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Navbar expand="lg" className="NavbarCollapse">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Projects</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <NavDropdown title="Contacts" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 }
 
