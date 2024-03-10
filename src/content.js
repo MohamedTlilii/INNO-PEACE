f587414be6156149fd5931b607950e11b2c32c84
MohamedTlili <Mohamedtlili1995@hotmail.com>
2024-03-10 23:40:31 +0100

    projects

---
+++ src/content.js
@@ -0,0 +1,78 @@
+const content = [
+  {
+    id: "48c60964-b5cc-5e14-90cc-dc29de769539",
+    image: "/assets/gabes.jpg",
+    title: "La Bibliothèque Humaine",
+    description1:
+      "La Bibliothèque Humaine (BH) privilégie la RENCONTRE et L'ÉCHANGE. Son principe est simple : vous êtes un lecteur face à un livre humain qui porte un titre. En fait c'est une personne.",
+    description2:
+      "Vous n'avez alors plus qu'à prêter l'oreille! Pendant une trentaine de minutes, ce livre humain vous fait partager son histoire et son expérience et répond à vos questionnements.",
+    description3:
+      "Vous pouvez poser toutes vos questions au Livre Humain sur sa DIFFÉRENCE et sur la potentielle DISCRIMINATION subie par la société, SANS TABOU ni PUDEUR mais dans le RESPECT MUTUEL.",
+    but: [
+      "• Faire face aux préjugés et stéréotypes et inciter le lecteur à une autocritique.",
+      "• Lutter contre le racisme, la discrimination et la violence, violence engendrée par le racisme, qui est lui-même engendré par un refus, une incompréhension de la différence.",
+    ],
+    histoire:
+      "Introduite en Tunisie pour la première fois en 2016 par INNOPEACE, la Bibliothèque Humaine est un concept qui fut créé à COPENHAGEN en 2000 par quatre danois, visant LA LUTTE CONTRE LA DISCRIMINATION en toutes ses formes.",
+    ajourdhui: [
+      "• la Bibliothèque Humaine s'est déroulée dans plus de 85 PAYS dépassant ainsi les frontières géographiques, culturelles et contextuelles.",
+      "• C'est dans cette logique qu'à travers INNOPEACE, la Bibliothèque Humaine s'est installée en Tunisie, avec déjà 38 éditions dans 7 gouvernorats Tunis, Ariana, Sousse, Monastir, Sfax, Ben Arous, Nabeul.",
+      "• INNO-PEACE, est le représentant officiel du projet Bibliothèque Humaine par 'HLO' : HUMAIN LIBRARY ORGANISATION' en Tunisie et en Afrique du Nord.",
+    ],
+
+    images: [
+      {
+        src: "/assets/nn.jpg",
+      },
+      {
+        src: "/assets/aaa.jpg",
+      },
+      {
+        src: "/assets/eeee.jpg",
+      },
+      {
+        src: "/assets/eeeeeee.jpg",
+      },
+    ],
+  },
+  {
+    id: "48c60964-b5cc-5e14-90cc-dc29de769539",
+    image: "/assets/gabes.jpg",
+    title: "La Bibliothèque Humaine",
+    description1:
+      "La Bibliothèque Humaine (BH) privilégie la RENCONTRE et L'ÉCHANGE. Son principe est simple : vous êtes un lecteur face à un livre humain qui porte un titre. En fait c'est une personne.",
+    description2:
+      "Vous n'avez alors plus qu'à prêter l'oreille! Pendant une trentaine de minutes, ce livre humain vous fait partager son histoire et son expérience et répond à vos questionnements.",
+    description3:
+      "Vous pouvez poser toutes vos questions au Livre Humain sur sa DIFFÉRENCE et sur la potentielle DISCRIMINATION subie par la société, SANS TABOU ni PUDEUR mais dans le RESPECT MUTUEL.",
+    but: [
+      "• Faire face aux préjugés et stéréotypes et inciter le lecteur à une autocritique.",
+      "• Lutter contre le racisme, la discrimination et la violence, violence engendrée par le racisme, qui est lui-même engendré par un refus, une incompréhension de la différence.",
+    ],
+    histoire:
+      "Introduite en Tunisie pour la première fois en 2016 par INNOPEACE, la Bibliothèque Humaine est un concept qui fut créé à COPENHAGEN en 2000 par quatre danois, visant LA LUTTE CONTRE LA DISCRIMINATION en toutes ses formes.",
+    ajourdhui: [
+      "• la Bibliothèque Humaine s'est déroulée dans plus de 85 PAYS dépassant ainsi les frontières géographiques, culturelles et contextuelles.",
+      "• C'est dans cette logique qu'à travers INNOPEACE, la Bibliothèque Humaine s'est installée en Tunisie, avec déjà 38 éditions dans 7 gouvernorats Tunis, Ariana, Sousse, Monastir, Sfax, Ben Arous, Nabeul.",
+      "• INNO-PEACE, est le représentant officiel du projet Bibliothèque Humaine par 'HLO' : HUMAIN LIBRARY ORGANISATION' en Tunisie et en Afrique du Nord.",
+    ],
+
+    images: [
+      {
+        src: "/assets/nn.jpg",
+      },
+      {
+        src: "/assets/aaa.jpg",
+      },
+      {
+        src: "/assets/eeee.jpg",
+      },
+      {
+        src: "/assets/eeeeeee.jpg",
+      },
+    ],
+  },
+];
+
+export default content;
--- src/page/Projects/Projects.js
+++ src/page/Projects/Projects.js
@@ -3,23 +3,35 @@ import "./Style.css";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import content from "../../content";
 
 function Projects() {
   return (
     <div className="projectsSection">
       <h1>Projects</h1>
       <Row xs={1} md={2} className="g-4">
-        {Array.from({ length: 7 }).map((_, idx) => (
-          <Col key={idx}>
+        {content.map((project) => (
+          <Col key={project.id}>
             <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Img variant="top" src={project.image} />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
+                <Card.Title>{project.title}</Card.Title>
+                <Card.Text>{project.description1}</Card.Text>
+                <Card.Text>{project.description2}</Card.Text>
+                <Card.Text>{project.description3}</Card.Text>
+                <h5>But:</h5>
+                <ul>
+                  {project.but.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </ul>
+                <p>{project.histoire}</p>
+                <h5>Ajourdhui:</h5>
+                <ul>
+                  {project.ajourdhui.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </ul>
               </Card.Body>
             </Card>
           </Col>
